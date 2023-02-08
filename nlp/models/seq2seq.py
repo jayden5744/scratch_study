014300fe7777207720b4427ead75ad334a4b4bc5
HSJeong5744 <jhs0256@gmail.com>
2023-02-08 21:45:07 +0900

    [Feat] Add Seq2Seq with scheduled sampling

--- nlp/models/seq2seq.py
+++ nlp/models/seq2seq.py
@@ -1,3 +1,6 @@
+import random
+from typing import Optional, Tuple, Union
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -28,9 +31,17 @@ class Encoder(nn.Module):
             batch_first=batch_first,
         )
 
-    def forward(self, enc_input: Tensor):
+        for m in self.modules():
+            if hasattr(m, "weight") and m.weight.dim() > 1:
+                nn.init.xavier_uniform_(m.weight.data)
+
+    def forward(
+        self,
+        enc_input: Tensor,
+        enc_hidden: Optional[Union[Tensor, Tuple[Tensor, Tensor]]] = None,
+    ):
         embeded = self.embedding(enc_input)
-        output, hidden = self.layers(embeded)
+        output, hidden = self.layers(embeded, enc_hidden)
         return output, hidden
 
     def select_mode(
@@ -93,7 +104,7 @@ class Decoder(nn.Module):
         bidirectional: bool = False,
     ) -> None:
         super().__init__()
-        self.embedding = nn.Embedding(hidden_size, output_size)
+        self.embedding = nn.Embedding(output_size, hidden_size)
         self.layers = self.select_mode(
             mode=mode,
             hidden_size=hidden_size,
@@ -106,11 +117,19 @@ class Decoder(nn.Module):
         self.linear = nn.Linear(hidden_size, output_size)
         self.softmax = nn.LogSoftmax(dim=1)
 
-    def forward(self, enc_input: Tensor, hidden):
-        embeded = self.embedding(enc_input)
+        for m in self.modules():
+            if hasattr(m, "weight") and m.weight.dim() > 1:
+                nn.init.xavier_uniform_(m.weight.data)
+
+    def forward(
+        self,
+        dec_input: Tensor,
+        hidden: Optional[Union[Tensor, Tuple[Tensor, Tensor]]] = None,
+    ):
+        embeded = self.embedding(dec_input)
         relu_embeded = F.relu(embeded)  # 논문에는 없는 것
-        output, hidden = self.layers(relu_embeded)
-        output = self.softmax(self.linear(output[0]))
+        output, hidden = self.layers(relu_embeded, hidden)
+        output = self.softmax(self.linear(output))
         return output, hidden
 
     def select_mode(
@@ -158,3 +177,43 @@ class Decoder(nn.Module):
 
         else:
             raise ValueError("param `mode` must be one of [rnn, lstm, gru]")
+
+
+class Seq2Seq(nn.Module):
+    def __init__(
+        self,
+        encoder: Encoder,
+        decoder: Decoder,
+        max_sequence_size: int,
+        vocab_size: int,
+    ) -> None:
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.max_sequence_size = max_sequence_size
+        self.vocab_size = vocab_size
+
+    def forward(
+        self,
+        enc_input: Tensor,
+        dec_input: Tensor,
+        teacher_forcing_rate: Optional[float] = 1.0,
+    ) -> Tensor:
+        enc_hidden = None
+        for i in range(self.max_sequence_size):
+            enc_input_i = enc_input[:, i]
+            _, enc_hidden = self.encoder(enc_input=enc_input_i, enc_hidden=enc_hidden)
+
+        decoder_output = torch.zeros(
+            dec_input.size(0), self.max_sequence_size, self.vocab_size
+        )
+        dec_hidden = enc_hidden
+        for i in range(self.max_sequence_size):
+            if i == 0 or random.random() <= teacher_forcing_rate:
+                dec_input_i = dec_input[:, i]
+            else:
+                dec_input_i = dec_output_i.topk(1)[1].sequeeze().detach()
+
+            dec_output_i, dec_hidden = self.decoder(dec_input_i, dec_hidden)
+            decoder_output[:, i, :] = dec_output_i
+        return decoder_output
