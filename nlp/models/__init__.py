3f9fb70eacb9da7428630986b7b1eb7fc195e173
HSJeong5744 <jhs0256@gmail.com>
2023-05-24 13:02:05 +0900

    [Feat] Init Transformer

---
+++ nlp/models/__init__.py
@@ -0,0 +1,3 @@
+from seq2seq import Seq2Seq
+from attention import Seq2SeqWithAttention
+from transformer import Transformer
---
+++ nlp/models/transformer.py
@@ -0,0 +1,8 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import Tensor
+
+
+class Transformer(nn.Module):
+    pass
